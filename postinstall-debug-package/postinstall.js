39d9e0bb5e704005b73dd8f7341a9fc431145b73
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-04-12 22:46:31 +0900

    feat: show "bin" command result (#4)
    * feat: detect global mode ( https://github.com/sounisi5011/nodejs-package-managers-debug--postinstall-script/pull/4/commits/b656fa804595599f351bfe8af7ce08f9f312979d )
    
    * feat: run the "bin" command for each package manager ( https://github.com/sounisi5011/nodejs-package-managers-debug--postinstall-script/pull/4/commits/aaed42a55e638c8f084fd191b6744eef5e053020 )
    
    * fix: use the "npm_execpath" environment variable instead of the command name ( https://github.com/sounisi5011/nodejs-package-managers-debug--postinstall-script/pull/4/commits/39fc918be705fd4d6f868970bd449dca97c46541 )
    
    * fix: assign `1` even if `process.exitCode` is `undefined` ( https://github.com/sounisi5011/nodejs-package-managers-debug--postinstall-script/pull/4/commits/a9bcf5a35d186683439c1d84cf391796736a7dce )
    
    * fix: use the `execFile()` function instead of the `exec()` function ( https://github.com/sounisi5011/nodejs-package-managers-debug--postinstall-script/pull/4/commits/2998fec3f49071ce67d6ca03748efb7da3addd97 )
    
        On Windows, the `exec()` function cannot execute a command specified by an absolute path.
    
    * fix: on Windows, use the `exec()` function instead of the `execFile()` function if the command name is not an absolute path ( https://github.com/sounisi5011/nodejs-package-managers-debug--postinstall-script/pull/4/commits/c0f4b31061476656d59c1db07039665d59a3e0c3 )

--- postinstall-debug-package/postinstall.js
+++ postinstall-debug-package/postinstall.js
@@ -1,35 +1,125 @@
-const { appendFileSync } = require('fs');
-const { inspect } = require('util');
+const { exec, execFile } = require('child_process');
+const { appendFile } = require('fs/promises');
+const path = require('path');
+const { inspect, promisify } = require('util');
+
+const execAsync = promisify(exec);
+const execFileAsync = promisify(execFile);
+
+let isGlobalMode = false;
+// In npm and pnpm, global mode can be detected by reading the "npm_config_global" environment variable.
+// Older npm defines this for local mode as well, so make sure it is equal to "'true'".
+if (process.env.npm_config_global === 'true') {
+  isGlobalMode = true;
+}
+// In Yarn v1, global mode can be detected by reading the "npm_config_argv" environment variable.
+// This value is a JSON string containing the original arguments passed to the "yarn" command.
+// It is parsed to check if the called subcommand is "yarn global add".
+const isYarn1 = (process.env.npm_config_user_agent || '')?.startsWith(
+  'yarn/1.',
+);
+if (isYarn1 && process.env.npm_config_argv) {
+  const npmArgv = JSON.parse(process.env.npm_config_argv);
+  if (Array.isArray(npmArgv?.original)) {
+    // Arguments include options.
+    // To ignore them, compare the position of the keywords "global" and "add" to check whether the subcommand is "yarn global add".
+    // Note: Strictly speaking, this logic is incorrect.
+    //       The value of options MUST be ignored.
+    //       For example, if the following command is executed:
+    //           yarn --cache-folder global add ...
+    //       This logic incorrectly determines that this is a "yarn global add" command,
+    //       because it does not know that the "global" keyword is the value of the "--cache-folder" option.
+    const globalPos = npmArgv?.original.indexOf('global');
+    const addPos = npmArgv?.original.indexOf('add');
+    if (0 <= globalPos && globalPos < addPos) {
+      isGlobalMode = true;
+    }
+  }
+}
+// Since Yarn v2, there is no global mode.
+// The "yarn global add" command has been replaced with the "yarn dlx" command.
+// see https://yarnpkg.com/getting-started/migration#use-yarn-dlx-instead-of-yarn-global
 
 const postinstallType =
   process.argv
     .map((arg) => /^--type\s*=(.+)$/.exec(arg)?.[1].trim())
     .findLast(Boolean) ?? process.env.POSTINSTALL_TYPE;
-const envs = {
-  cwd: process.cwd(),
-  ...Object.fromEntries(
-    Object.entries(process.env).filter(([key]) =>
-      /^(?:npm|yarn|pnpm|bun)_/i.test(key),
-    ),
-  ),
-};
-if (postinstallType) console.log(postinstallType);
-console.log(envs);
 
-const { GITHUB_STEP_SUMMARY } = process.env;
-if (GITHUB_STEP_SUMMARY)
-  appendFileSync(
-    GITHUB_STEP_SUMMARY,
-    [
-      `<details${Object.keys(envs).length < 30 ? ' open' : ''}>`,
-      ...(postinstallType ? [`<summary>${postinstallType}</summary>`] : []),
-      '',
-      '```js',
-      inspect(envs),
-      '```',
-      '',
-      '</details>',
-      '',
-      '',
-    ].join('\n'),
-  );
+/**
+ * @param {string} commandName
+ * @returns {string[]}
+ */
+function packageManagerCommand(commandName) {
+  const execpathIsJS =
+    process.env.npm_execpath &&
+    /\.[cm]?js$/.test(path.extname(process.env.npm_execpath));
+  return execpathIsJS
+    ? // Sometimes there are multiple versions of a package manager on a user's system, such as when using Corepack.
+      // In this case, the exec function may call another package manager that is in a different path than the running package manager.
+      // To avoid this, use the environment variable "npm_execpath".
+      [process.execPath, process.env.npm_execpath]
+    : [commandName];
+}
+
+/**
+ * @param {string} command
+ * @param {string[]} args
+ */
+async function crossExec(command, args) {
+  if (process.platform !== 'win32' || path.isAbsolute(command)) {
+    return await execFileAsync(command, args);
+  }
+  // Note: This is bad code because it does not quote each argument.
+  //       However, this is not a problem because the arguments of the commands executed within this script do not need to be quoted.
+  return await execAsync([command, ...args].join(' '));
+}
+
+(async () => {
+  const binCommand = isYarn1
+    ? packageManagerCommand('yarn').concat(isGlobalMode ? 'global' : [], 'bin')
+    : (process.env.npm_config_user_agent || '')?.startsWith('pnpm/')
+    ? packageManagerCommand('pnpm').concat(
+        'bin',
+        isGlobalMode ? '--global' : [],
+      )
+    : packageManagerCommand('npm').concat(
+        'bin',
+        isGlobalMode ? '--global' : [],
+      );
+  const debugData = {
+    cwd: process.cwd(),
+    isGlobalMode,
+    [binCommand.join(' ')]: await crossExec(
+      binCommand[0],
+      binCommand.slice(1),
+    ).catch((error) => ({ error })),
+    env: Object.fromEntries(
+      Object.entries(process.env).filter(([key]) =>
+        /^(?:npm|yarn|pnpm|bun)_/i.test(key),
+      ),
+    ),
+  };
+  if (postinstallType) console.log(postinstallType);
+  console.log(debugData);
+
+  const { GITHUB_STEP_SUMMARY } = process.env;
+  if (GITHUB_STEP_SUMMARY)
+    await appendFile(
+      GITHUB_STEP_SUMMARY,
+      [
+        `<details${Object.keys(debugData.env).length < 30 ? ' open' : ''}>`,
+        ...(postinstallType ? [`<summary>${postinstallType}</summary>`] : []),
+        '',
+        '```js',
+        inspect(debugData),
+        '```',
+        '',
+        '</details>',
+        '',
+        '',
+      ].join('\n'),
+    );
+})().catch((error) => {
+  if (!process.exitCode) process.exitCode = 1;
+  console.error(error);
+});
