cc8d44138cd007d895eab7908106e84f6cc0903c
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-05-23 12:01:08 +0900

    fix(./postinstall-debug-package/utils/get-installation-path.ts): use the `npm_config_local_prefix` and `INIT_CWD` environment variables for local mode

--- postinstall-debug-package/utils/get-installation-path.ts
+++ postinstall-debug-package/utils/get-installation-path.ts
@@ -4,42 +4,71 @@ import { promisify } from 'util';
 
 import usedPM from 'used-pm';
 
+import { isGlobalMode } from './is-global-mode';
+
 const execAsync = promisify(child_process.exec);
 
-/**
- * @see https://github.com/go-task/go-npm/blob/v0.1.17/src/common.js#L23-L60
- */
 export async function getInstallationPath(): Promise<string> {
-  const { stdout, stderr } = await execAsync('npm bin').catch(() => ({
-    stdout: '',
-    stderr: '',
-  }));
+  const env = process.env;
 
-  let dir = null;
-  if (stderr || !stdout || stdout.length === 0) {
-    const env = process.env;
-
-    const packageManager = usedPM();
-
-    if (env && env['npm_config_prefix']) {
-      dir = path.join(env['npm_config_prefix'], 'bin');
-    } else if (env && env['npm_config_local_prefix']) {
-      dir = path.join(
-        env['npm_config_local_prefix'],
-        path.join('node_modules', '.bin'),
-      );
-    } else if (packageManager?.name.toLowerCase() === 'pnpm') {
-      dir = path.join(process.cwd(), 'node_modules', '.bin');
-    } else {
-      throw new Error('Error finding binary installation directory');
+  if (!isGlobalMode) {
+    // If the "npm_config_local_prefix" environment variable exists, use it.
+    if (env['npm_config_local_prefix']) {
+      return path.join(env['npm_config_local_prefix'], 'node_modules/.bin');
     }
-  } else {
-    dir = stdout.trim();
-  }
 
-  dir = dir.replace(
-    /node_modules.*[\/\\]\.bin/,
-    path.join('node_modules', '.bin'),
-  );
-  return dir;
+    // If the "npm_config_local_prefix" environment variable does not exist, use the current working directory and the "INIT_CWD" environment variable instead.
+    // The same parent directory path that is contained in both the current working directory and "INIT_CWD" can be used.
+    if (env['INIT_CWD']) {
+      const cwdPathItems = process.cwd().split(path.sep);
+      const initCwdPathItems = env['INIT_CWD'].split(path.sep);
+      for (
+        let i = 0;
+        i < Math.max(cwdPathItems.length, initCwdPathItems.length);
+        i++
+      ) {
+        if (cwdPathItems[i] !== initCwdPathItems[i]) {
+          const sameParentPathItems = cwdPathItems.slice(0, i);
+          sameParentPathItems.push('node_modules', '.bin');
+          return sameParentPathItems.join(path.sep);
+        }
+      }
+    }
+
+    throw new Error('Error finding binary installation directory');
+  } else {
+    /**
+     * @see https://github.com/go-task/go-npm/blob/v0.1.17/src/common.js#L23-L60
+     */
+    const { stdout, stderr } = await execAsync('npm bin').catch(() => ({
+      stdout: '',
+      stderr: '',
+    }));
+
+    let dir = null;
+    if (stderr || !stdout || stdout.length === 0) {
+      const packageManager = usedPM();
+
+      if (env && env['npm_config_prefix']) {
+        dir = path.join(env['npm_config_prefix'], 'bin');
+      } else if (env && env['npm_config_local_prefix']) {
+        dir = path.join(
+          env['npm_config_local_prefix'],
+          path.join('node_modules', '.bin'),
+        );
+      } else if (packageManager?.name.toLowerCase() === 'pnpm') {
+        dir = path.join(process.cwd(), 'node_modules', '.bin');
+      } else {
+        throw new Error('Error finding binary installation directory');
+      }
+    } else {
+      dir = stdout.trim();
+    }
+
+    dir = dir.replace(
+      /node_modules.*[\/\\]\.bin/,
+      path.join('node_modules', '.bin'),
+    );
+    return dir;
+  }
 }
